cc5f212e08ef4de97267ab185129fc11bc281bc7
WalaSTH <macielsalguero@gmail.com>
2023-08-11 19:06:00 -0300

    other models in django admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
 
-from .models import Room
+from .models import Room, User, SpotifyToken
 
 class RoomAdmin(admin.ModelAdmin):
     list_display = ('code', 'host', 'password', 'room_name', 'guest_pause',
                   'guest_manage_queue', 'guest_chat', 'guest_skip',
                   'private_room','show_lobby','guest_add_queue')
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'username', 'password', 'verified',
+                  'authenticated', 'room')
+
+class SpotifyTokenAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created_at', 'access_token', 'refresh_token', 'expires_in',
+                  'token_type')
+
 # Register your models here.
 
 admin.site.register(Room, RoomAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(SpotifyToken, SpotifyTokenAdmin)
