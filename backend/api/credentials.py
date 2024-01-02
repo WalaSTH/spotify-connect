63ddeae488ccc8ead9ef1bd72c7e0e912ce516a0
WalaSTH <macielsalguero@gmail.com>
2024-01-02 17:46:41 -0300

    credentials

--- backend/api/credentials.py
+++ backend/api/credentials.py
@@ -1,3 +1,4 @@
 CLIENT_ID = "d7f7d89859114f3cb4b6a880a0845681"
 CLIENT_SECRET = "b35b6035341e43fa8027d06493ee3465"
-REDIRECT_URI = "http://127.0.0.1:8000/api/spotify-redirect"
+#REDIRECT_URI = "http://127.0.0.1:8000/api/spotify-redirect"
+REDIRECT_URI = "http://18.223.74.214/api/spotify-redirect"
