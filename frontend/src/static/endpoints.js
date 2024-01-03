3e76f579ebdcee994fa75daa7eed96f17c0c5556
WalaSTH <macielsalguero@gmail.com>
2024-01-03 13:55:06 -0300

    new endpoints

--- frontend/src/static/endpoints.js
+++ frontend/src/static/endpoints.js
@@ -1,3 +1,6 @@
 const userInRoomEndpoint = "http://127.0.0.1:8000/api/user-in-room";
 
-export const BASE_BACKEND = "http://18.223.74.214";
+export const BASE_BACKEND = "https://spotifyconnect.cordobadigital.com.ar";
+
+//https://spotifyconnect.cordobadigital.com.ar/
+//http://18.223.74.214
