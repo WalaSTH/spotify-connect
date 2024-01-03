bab821207f2e7b1010055315b26ba4cfdc7a7de7
WalaSTH <macielsalguero@gmail.com>
2024-01-03 15:55:53 -0300

    settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -169,8 +169,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000',
     'http://localhost:3000',
-    "http://localhost:3000",
-    'http://localhost:3000',
     'http://127.0.0.1:3000',
     'https://accounts.spotify.com',
     'http://frontend:3000',
@@ -179,8 +177,27 @@ CORS_ALLOWED_ORIGINS = [
     'https://main--musical-mermaid-a74f78.netlify.app'
 ]
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
 
-
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 CORS_ALLOWED_ORIGINS2 = ["*"]
 
